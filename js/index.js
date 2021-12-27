3d95c94352e394fbd1b2e2c206b32e38b45f8714
Lee MeNGUELE <lee.menguele@trustinnotech.agency>
2021-12-27 11:21:55 +0100

    create the changing image effect onclick with js

--- js/index.js
+++ js/index.js
@@ -7,9 +7,20 @@ cart.addEventListener('click', function () {
     cart_container.classList.toggle('show');
 });
 
-
 // change the image in the hero section
-const carouselImage = document.querySelector('#carouselImage');
-const carouselItem = document.querySelectorAll('.carouselItem');
+var carouselImage = document.querySelector('#carouselImage');
+var carouselItem = document.querySelectorAll('.carouselItem');
 
-carouselItem.forEach()
+
+carouselItem.forEach(image => {
+    image.addEventListener('click', function(event) {
+        if (document.querySelectorAll('.active .carouselItem').length == 1) {
+            image.parentElement.classList.remove('active');
+            image.classList.remove('active');
+        } else {
+            image.parentElement.classList.add('active');
+            image.classList.add('active');
+        }
+        carouselImage.src = image.src;
+    })
+})
